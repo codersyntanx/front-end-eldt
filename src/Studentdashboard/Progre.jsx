23103140cebd305dcb25c9a70e86c15dfefe2b7b
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-01-12 18:23:44 +0500

    public

--- src/Studentdashboard/Progre.jsx
+++ src/Studentdashboard/Progre.jsx
@@ -2,8 +2,30 @@
 function Progre(){
     return(
         <>
-        hello from progress
-        </>
+ <div className="main-contain-regist">
+        
+        <div className="card-head">
+        Registration
+        </div>
+        <div className="card-body">
+          
+            <form>
+            <label className="foam-label">First name</label>
+            <input className="registinput" type="text" placeholder="Name"/>
+            <label className="foam-label">Middle name</label>
+            <input className="registinput" type="text" placeholder="Name"/>
+            <label className="foam-label">Last name</label>
+            <input className="registinput" type="text" placeholder="Name"/>
+            <label className="foam-label">Date of birth</label>
+            <input className="registinput" type="text" placeholder="password"/>
+            <label className="foam-label">New password</label>
+            <input className="registinput" type="text" placeholder="password"/>
+            <label className="foam-label">Confirm password</label>
+            <input className="registinput" type="text" placeholder="password"/>
+            </form>
+            <button className="btn-warning">Save</button>
+        </div>
+    </div>        </>
     )
 }
 export default Progre
