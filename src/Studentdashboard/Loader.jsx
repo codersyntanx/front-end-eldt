fe91d8b2426ac0efe60b14fa1ff8d42eec2bdbe6
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-02-03 15:19:31 +0500

    update

--- src/Studentdashboard/Loader.jsx
+++ src/Studentdashboard/Loader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Loader.css'; // Import CSS for styling
-import sitelogo from "./LOGO ELDT 2024 04 Artboard 5.svg"
+import sitelogo from "./loaderlogo.png"
 import loaderimage from "./loaderimage.png"
 const Loader = () => {
   return (
     <div className='maincontainer'>
       {/* <span className="loader "></span> */}
-      <img src={sitelogo} alt='logo'  style={{width:"150px",position:"absolute"}}/>
+      <img src={sitelogo} alt='logo'  style={{position:"absolute"}}/>
       <img src={loaderimage} alt='logo' className='loadermain' />
 
     </div>
