d7a3ee1b41376abe880b019e92399bbe41de6635
Muhammad Saad Afzal <152029259+codersyntanx@users.noreply.github.com>
2024-02-07 22:50:12 +0500

    update

--- src/components/Home/LeftHeroContent.jsx
+++ src/components/Home/LeftHeroContent.jsx
@@ -153,7 +153,7 @@ function LeftHeroContent(...props) {
         behavior: "smooth", });
     }else{
       window.scrollTo({
-        top: window.scrollY + 1200,
+        top: window.scrollY + 1100,
         behavior: "smooth", });
     }
      
